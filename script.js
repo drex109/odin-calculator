57068c9d2cf59445a2aeb901bd3e300a5b16ad03
drex109 <96746274+drex109@users.noreply.github.com>
2025-05-26 13:18:28 -0600

    created initial functions for simple math

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(x, y){
+    return x + y;
+}
+function subtract(x, y){
+    return x - y;
+}
+function multiply(x, y) {
+    return x * y;
+}
+function divide(x, y) {
+    return x / y;
+}
+console.log(add(2, 3))
+console.log(subtract(2, 3))
+console.log(multiply(2, 3))
+console.log(divide(2, 3))
