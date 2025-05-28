7be6f603f2c0088630b7b7cf3acd34f7d71999ed
drex109 <96746274+drex109@users.noreply.github.com>
2025-05-28 16:17:36 -0600

    built out calculator display

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ console.log(add(2, 3))
 console.log(subtract(2, 3))
 console.log(multiply(2, 3))
 console.log(divide(2, 3))
+
+function operate(x, operator, y) {
+    switch(operator) {
+        case '+':
+            add(x, y);
+        case '-':
+            subtract(x, y);
+        case '*':
+            multiply(x, y);
+        case '/':
+            divide(x, y);
+    }
+}
